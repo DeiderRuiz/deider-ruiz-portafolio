952e90579db32116bf5f5e4ef443abfc9bcb6eec
DeiderRuiz <ruizdeider09@gmail.com>
2025-10-06 15:04:56 -0500

    Instalación de Vue gtag

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 import './styles.css'
 
 import App from './App.vue'
@@ -11,6 +12,13 @@ const pinia = createPinia() // crear instancia pinia
 
 app.use(pinia) // registrar pinia
 app.use(router) // registrar router vue
+app.use(
+  VueGtag,
+  {
+    config: { id: 'G-XXXXXXXXXX' }, // <-- tu ID de medición de Google Analytics 4
+  },
+  router,
+)
 
 // usar store pasando la instancia pinia explícitamente
 const profile = useProfileStore(pinia)
